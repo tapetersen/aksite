1570fbb4978fd7f2f6b1e15417ca9069689dc93f
Tobias Petersen <tobias.alex.petersen@gmail.com>
2011-04-03 17:43:28 +0200

    Fixed whitespace errors and added imageblocks for media-tag

--- urls.py
+++ urls.py
@@ -16,9 +16,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-                       
     url(r'', include('feincms.urls')),
 )
-    # media served if DEBUG = True
+# media served if DEBUG = True
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
