56fe1ba061546f364a89773991fc75da81f5e510
Sam Persson <samiljan@gmail.com>
2011-12-01 13:11:20 +0100

    Added support for sub-pages (one level)

--- app/jinja.py
+++ app/jinja.py
@@ -1,17 +1,43 @@
 from feincms.module.page.models import Page
-from models import Signup
+from collections import OrderedDict, namedtuple
 
 __all__ = ["navigation", "is_equal_or_parent_of"]
 
-def navigation(request, parent=None):
-    pages = Page.objects.active().filter(in_navigation=True, parent=parent)
+PageTuple = namedtuple("PageTuple", "tree_id lft rght title parent url children")
+
+def navigation(request):
+    """Get all pages with one query, then aranges in a tree."""
+    
+    pages = Page.objects.active().filter(in_navigation=True)
     if not request.user.is_authenticated():
         pages = pages.filter(require_login=False)
-    return pages
+        
+    tree = []
+    tree_dict = {None:tree} # References the same list
+    
+    page_dict = OrderedDict() # To keep order of the pages
+    for p in pages:
+        page_dict[p.id] = PageTuple(tree_id=p.tree_id,
+                                         lft=p.lft,
+                                         rght=p.rght, 
+                                         title=p.title, 
+                                         parent=p.parent.id if p.parent else None,
+                                         url=p.get_absolute_url(), 
+                                         children=[])
+
+    
+    while page_dict:
+        for i, page in page_dict.items():
+            if page.parent in tree_dict:
+                tree_dict[page.parent].append(page)
+                tree_dict[i] = page.children
+                del page_dict[i]
+        import logging
+        logging.warning(page_dict, extra=dict(request=request))
+    
+    logging.warning(tree, extra=dict(request=request))
+    return tree
 
 def is_equal_or_parent_of(page1, page2):
-    try:
-        return page1.tree_id == page2.tree_id and page1.lft <= page2.lft and page1.rght >= page2.rght
-    except AttributeError:
-        return False
+    return page1.tree_id == page2.tree_id and page1.lft <= page2.lft and page1.rght >= page2.rght
 
