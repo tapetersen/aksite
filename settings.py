14b136ea3d57add536a52abce6324e0fd1d3e4ac
Sam Persson <samiljan@gmail.com>
2011-12-01 16:43:58 +0100

    Added 404 logging and changed apple-touch-icon

--- settings.py
+++ settings.py
@@ -141,6 +141,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'sentry.client.middleware.Sentry404CatchMiddleware',
     'compatibility.XUACompatibleMiddleware',
 )
 
